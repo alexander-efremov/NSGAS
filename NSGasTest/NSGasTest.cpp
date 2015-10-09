c57ff503cc87e3766d92a2d71715b2fc9bb1d48b
Dampir <aedampir@gmail.com>
2015-10-09 22:46:19 +0700

    Initial commit for tests

--- NSGasTest/NSGasTest.cpp
+++ NSGasTest/NSGasTest.cpp
@@ -1,22 +1,11 @@
 #include <gtest/gtest.h>
-#include <iostream>
 
 using namespace std;
 using namespace ::testing;
 
-int factorian(int n)
-{
-	if (n == 0 || n == 1)
-		return 1;
-	else
-		return n * factorian(n - 1);
-}
 
-TEST(factorianTest, simpleTest)
+TEST(nsgas, main_test)
 {
-	ASSERT_EQ(1, factorian(0));
-	ASSERT_EQ(1, factorian(1));
-	ASSERT_EQ(2, factorian(2));
 }
 
 int main(int ac, char* av [])
